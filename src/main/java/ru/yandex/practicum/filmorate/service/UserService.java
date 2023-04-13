346550b4c89d1512084e8363baac7f1d444f4312
IlyaLyutin <ilyalyutin@gmail.com>
2023-04-13 13:56:12 +0300

    feat: add classes: User, Film, add Controllers add Services

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,65 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import ru.yandex.practicum.filmorate.exception.FindUserException;
+import ru.yandex.practicum.filmorate.exception.ValidateUserException;
+import ru.yandex.practicum.filmorate.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
+@Getter
+@Setter
+public class UserService {
+    private Map<Integer, User> users;
+    int id;
+
+    public UserService() {
+        id = 0;
+        users = new HashMap<>();
+    }
+
+    public Collection<User> getUsers() {
+        return users.values();
+    }
+
+    public User createUser(User user) throws ValidateUserException {
+        user.setId(++id);
+        checkName(user);
+        checkLogin(user);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    private void checkLogin(User user) throws ValidateUserException {
+        if (user.getLogin().contains(" ")) {
+            throw new ValidateUserException("Name не должен содержать пробелы");
+        }
+    }
+
+    private void checkName(User user) {
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+    }
+
+    public User updateUser(User user) throws ValidateUserException, FindUserException {
+        checkId(user);
+        checkName(user);
+        checkLogin(user);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    private void checkId(User user) throws FindUserException {
+        if (!users.containsKey(user.getId())) {
+            throw new FindUserException("Пользователь с id: " + user.getId() + " не найден");
+        }
+    }
+}
