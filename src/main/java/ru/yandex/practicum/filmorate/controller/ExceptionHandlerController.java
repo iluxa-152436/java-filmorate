d5d7fb9541e88f5cdd4be0e176833411cc2b4779
IlyaLyutin <ilyalyutin@gmail.com>
2023-05-25 13:45:34 +0300

    fix: add FindMpaRatingException.class, FindGenreException.class in ExceptionHandlerController

--- src/main/java/ru/yandex/practicum/filmorate/controller/ExceptionHandlerController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ExceptionHandlerController.java
@@ -7,10 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.exception.FindFilmException;
-import ru.yandex.practicum.filmorate.exception.FindUserException;
-import ru.yandex.practicum.filmorate.exception.ValidateFilmException;
-import ru.yandex.practicum.filmorate.exception.ValidateUserException;
+import ru.yandex.practicum.filmorate.exception.*;
 import ru.yandex.practicum.filmorate.model.ApiErrorMessage;
 
 import javax.validation.ConstraintViolationException;
@@ -18,9 +15,13 @@ import javax.validation.ConstraintViolationException;
 @Slf4j
 @RestControllerAdvice
 public class ExceptionHandlerController {
-    @ExceptionHandler(value = {FindFilmException.class, FindUserException.class, EmptyResultDataAccessException.class})
+    @ExceptionHandler(value = {FindFilmException.class,
+            FindUserException.class,
+            EmptyResultDataAccessException.class,
+            FindMpaRatingException.class,
+            FindGenreException.class})
     public ResponseEntity<ApiErrorMessage> handleNotFoundException(Exception exception) {
-        log.info("Получен статус 404 Not found {}", exception.getMessage(), exception);
+        log.debug("Получен статус 404 Not found {}", exception.getMessage(), exception);
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ApiErrorMessage(exception.getMessage()));
@@ -28,7 +29,7 @@ public class ExceptionHandlerController {
 
     @ExceptionHandler(value = {ValidateFilmException.class, ValidateUserException.class})
     public ResponseEntity<ApiErrorMessage> handleValidateException(Exception exception) {
-        log.info("Получен статус 400 Bad request {}", exception.getMessage(), exception);
+        log.debug("Получен статус 400 Bad request {}", exception.getMessage(), exception);
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ApiErrorMessage(exception.getMessage()));
@@ -36,7 +37,7 @@ public class ExceptionHandlerController {
 
     @ExceptionHandler(value = {MethodArgumentNotValidException.class, ConstraintViolationException.class})
     public ResponseEntity<ApiErrorMessage> handleArgumentNotValidException(MethodArgumentNotValidException exception) {
-        log.info("Получен статус 400 Bad request {}", exception.getMessage(), exception);
+        log.debug("Получен статус 400 Bad request {}", exception.getMessage(), exception);
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ApiErrorMessage(exception.getFieldError().getDefaultMessage()));
@@ -44,7 +45,7 @@ public class ExceptionHandlerController {
 
     @ExceptionHandler
     public ResponseEntity<ApiErrorMessage> handleThrowable(final Throwable exception) {
-        log.info("Получен статус 500 Internal server error {}", exception.getMessage(), exception);
+        log.debug("Получен статус 500 Internal server error {}", exception.getMessage(), exception);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ApiErrorMessage("Произошла непредвиденная ошибка."));
