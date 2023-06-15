2ff4cd45b62b3b11b8fde4dfa23f359b3fdd759a
Alexander Frolov <terofits@yandex.ru>
2023-06-15 18:16:18 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -84,7 +84,7 @@ public class FilmController {
                 throw new IllegalArgumentException("Incorrect sorting order");
         }
     }
-  
+
     @GetMapping("/common")
     public List<Film> getCommonFilms(@RequestParam Integer userId, @RequestParam Integer friendId) {
         return filmService.getCommonFilms(userId, friendId);
