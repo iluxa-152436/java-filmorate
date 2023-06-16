3c50e1f00a9d5be42fa2f367be0ac954e5b183ae
IlyaLyutin <ilyalyutin@gmail.com>
2023-06-16 22:17:24 +0300

    fix: remove Film constructors

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,9 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import ru.yandex.practicum.filmorate.validator.ReleaseDateConstraint;
 
 import javax.validation.constraints.NotBlank;
@@ -12,12 +9,12 @@ import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 import static ru.yandex.practicum.filmorate.Constants.MAX_FILM_DESCRIPTION_LENGTH;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Builder
 public class Film {
@@ -33,44 +30,8 @@ public class Film {
     private LocalDate releaseDate;
     @Positive(message = "duration should be greater than 0")
     private int duration;
-    private Set<Genre> genres;
+    private Set<Genre> genres = new HashSet<>();
     private MpaRating mpa;
     @EqualsAndHashCode.Exclude
-    private Set<Director> directors;
-
-    public Film(int id,
-                String name,
-                String description,
-                LocalDate releaseDate,
-                int duration,
-                Set<Genre> genres,
-                MpaRating mpa,
-                Set<Director> directors) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.releaseDate = releaseDate;
-        this.duration = duration;
-        this.genres = Objects.requireNonNullElseGet(genres, HashSet::new);
-        this.directors = Objects.requireNonNullElseGet(directors, HashSet::new);
-        this.mpa = mpa;
-    }
-
-    public Film(int id,
-                String name,
-                String description,
-                LocalDate releaseDate,
-                int duration,
-                Set<Genre> genres,
-                MpaRating mpa) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.releaseDate = releaseDate;
-        this.duration = duration;
-        this.genres = Objects.requireNonNullElseGet(genres, HashSet::new);
-        this.mpa = mpa;
-    }
-
-
+    private Set<Director> directors = new HashSet<>();
 }
